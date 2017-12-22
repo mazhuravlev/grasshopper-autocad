b4489cef155c0e08751e6048b74b692bdeaa30d8
Mikhail Zhuravlev <zhuravlevma@pik.ru>
2017-12-22 13:46:41 +0300

    Add project files.

---
+++ Autocad/AutocadInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Autocad
+{
+  public class AutocadInfo : GH_AssemblyInfo
+  {
+    public override string Name
+    {
+      get
+      {
+        return "Autocad";
+      }
+    }
+    public override Bitmap Icon
+    {
+      get
+      {
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        return null;
+      }
+    }
+    public override string Description
+    {
+      get
+      {
+        //Return a short string describing the purpose of this GHA library.
+        return "";
+      }
+    }
+    public override Guid Id
+    {
+      get
+      {
+        return new Guid("81c6097d-c3ba-488a-886c-b78abbf85333");
+      }
+    }
+
+    public override string AuthorName
+    {
+      get
+      {
+        //Return a string identifying you or your company.
+        return "";
+      }
+    }
+    public override string AuthorContact
+    {
+      get
+      {
+        //Return a string representing your preferred contact details.
+        return "";
+      }
+    }
+  }
+}
